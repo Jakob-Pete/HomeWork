0df782cec13079115609e6ff1d6e3a4dae37fa14
me <me.2>
2022-09-20 10:26:37 -0600

    test to see whats being added

---
+++ September/code challenges/6.2 code challenge 9:20:22.playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+var greeting = "Hello, playground"
+func returnEvenNum(num: Int) -> Int {
+    let nEvenNum = (num - 1) * 2
+    return nEvenNum
+}
+returnEvenNum(num: 3)
+returnEvenNum(num: 100)
+
+
+//func double(positioin: Int) -> Int {
+//    var int = 0
+//    
+//    for _ in 1..<positioin {
+//        int += 2
+//    }
+//    return int
+//}
+//double(positioin: 8)
+//print(double(positioin: 3.dump))
